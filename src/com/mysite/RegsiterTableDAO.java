489b60b1bc678004a41914cb451eaaaed8e473ad
Ashu@test <69616825+Ashu-Test@users.noreply.github.com>
2020-08-14 22:48:56 +0530

    Update RegsiterTableDAO.java

--- src/com/mysite/RegsiterTableDAO.java
+++ src/com/mysite/RegsiterTableDAO.java
@@ -29,6 +29,28 @@ public class RegsiterTableDAO {
     private int ticket;
     private String picName;
   private String mobile;
+    
+    
+    
+     
+  
+  private String base64Image;
+
+    public String getBase64Image() {
+        return base64Image;
+    }
+
+    public void setBase64Image(String base64Image) {
+        this.base64Image = base64Image;
+    }
+
+    
+    
+    
+    
+    
+    
+    
 
    public String getMobile() {
         return mobile;
