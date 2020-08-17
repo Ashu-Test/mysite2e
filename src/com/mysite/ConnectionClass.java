f420e436f31f7a171513689d583d89c9c97e3d2e
Ashu@test <69616825+Ashu-Test@users.noreply.github.com>
2020-08-17 10:48:37 +0530

    Update ConnectionClass.java

--- src/com/mysite/ConnectionClass.java
+++ src/com/mysite/ConnectionClass.java
@@ -8,7 +8,7 @@ package com.mysite;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+import com.mysql.cj.jdbc.MysqlDataSource;
 /**
  *
  * @author ashut  mysiteDB
@@ -18,13 +18,13 @@ public class ConnectionClass {
     
     private static Connection conn;
 
-    public static Connection getConnection(){
+    public static Connection getMyConnection(){
 
         try {
 
             if(conn==null){
             try {
-                Class.forName("com.mysql.jdbc.Driver");
+                Class.forName("com.mysql.cj.jdbc.Driver");
                 } catch (ClassNotFoundException e) {
 			System.out.println("Error. Driver class not found: " + e);
 		}
@@ -32,7 +32,19 @@ public class ConnectionClass {
                 
                                                               //jws-app-mysql
                 try {
-                conn= DriverManager.getConnection("jdbc:mysql://jws-app-mysql:3306/db?useSSL=false&autoReconnect=true","ashu","ashu14mysql");
+               
+	MysqlConnectionPoolDataSource ds=new MysqlConnectionPoolDataSource();
+			ds.setURL("jdbc:mysql://jws-app-mysql:3306/db?useSSL=false&autoReconnect=true");
+			
+			ds.setUser("ashu");
+			ds.setPassword("ashu14mysql");
+			
+			conn=ds.getConnection();
+			
+			
+			
+			
+			//conn= DriverManager.getConnection("jdbc:mysql://jws-app-mysql:3306/db?useSSL=false&autoReconnect=true","ashu","ashu14mysql");
                } catch (SQLException e) {
 			System.out.println("Error. Connection problem: " + e);
 			
