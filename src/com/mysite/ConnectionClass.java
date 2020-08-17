276a64df871a21bf5fbccc7af38c2c70c411d93a
Ashu@test <69616825+Ashu-Test@users.noreply.github.com>
2020-08-17 11:59:58 +0530

    Update ConnectionClass.java

--- src/com/mysite/ConnectionClass.java
+++ src/com/mysite/ConnectionClass.java
@@ -10,6 +10,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 //import com.mysql.cj.jdbc.MysqlDataSource;
 //import com.mysql.jdbc.jdbc2.optional.*;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NameNotFoundException;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+
+
+
 /**
  *
  * @author ashut  mysiteDB
@@ -19,7 +29,38 @@ public class ConnectionClass {
     
     private static Connection conn;
 
-    public static Connection getMyConnection(){
+   
+	
+	
+	
+	
+	
+	private static DataSource lookupDataSource() {
+		try {
+			Context initialContext = new InitialContext();
+			try {
+				return (DataSource) initialContext.lookup(System.getenv("DB_JNDI"));
+			} catch (NameNotFoundException e) {
+				Context envContext = (Context) initialContext.lookup("java:comp/env"); // Tomcat places datasources
+																						// inside java:comp/env
+				return (DataSource) envContext.lookup(System.getenv("DB_JNDI"));
+			}
+		} catch (NamingException e) {
+			throw new RuntimeException("Could not look up datasource", e);
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static Connection getMyConnection(){
 
         try {
 
@@ -45,7 +86,14 @@ public class ConnectionClass {
 			  */
 			
 			
-			conn= DriverManager.getConnection("jdbc:mysql://jws-app-mysql:3306/db?useSSL=false&autoReconnect=true","ashu","ashu14mysql");
+			
+			DataSource dataSource = lookupDataSource();
+
+		conn = dataSource.getConnection();
+			
+			
+			
+			//conn= DriverManager.getConnection("jdbc:mysql://jws-app-mysql:3306/db?useSSL=false&autoReconnect=true","ashu","ashu14mysql");
                } catch (SQLException e) {
 			System.out.println("Error. Connection problem: " + e);
 			
